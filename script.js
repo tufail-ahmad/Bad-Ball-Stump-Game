9970704c0299198e6ad4ce6b219b2c2a37dd3be3
Tufail Ahmad <tufailahmadashrafi@gmail.com>
2024-06-24 21:34:01 +0530

    Create Game add HTML, CSS and JS

---
+++ script.js
@@ -0,0 +1,10 @@
+function computerMove() {
+  let randomNumber = Math.random() * 3;
+  if (randomNumber > 0 && randomNumber <= 1) {
+    return "Bat";
+  } else if (randomNumber > 1 && randomNumber <= 2) {
+    return "Ball";
+  } else {
+    return "Stump";
+  }
+}
